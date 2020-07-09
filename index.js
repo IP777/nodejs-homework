fa71faeb870d58808cb3eb18c64d97840a099111
IP777 <ivanov_piter@ukr.net>
2020-07-09 15:03:44 +0300

    edits-4

--- index.js
+++ index.js
@@ -18,12 +18,6 @@ app.use(cors());
 
 app.use("/api/contacts/", contactRouter);
 
-// app.use((err, req, res, next) => {
-// 	const { message, status } = err;
-
-// 	res.status(status || 500).send(message);
-// });
-
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}!`);
 });
