d7dfcd33bd747f0d876cf6035cf15681e20ed9fa
IP777 <ivanov_piter@ukr.net>
2020-07-31 18:33:35 +0300

    edits-4

--- api/upload/upload.router.js
+++ api/upload/upload.router.js
@@ -26,4 +26,22 @@ uploadRouter.post(
 	}
 );
 
+const imagemin = require("imagemin");
+const imageminJpegtran = require("imagemin-jpegtran");
+const imageminPngquant = require("imagemin-pngquant");
+
+async function minificationFile(req, res, next) {
+	const files = await imagemin(["images/*.{jpg,png}"], {
+		destination: "build/images",
+		plugins: [
+			imageminJpegtran(),
+			imageminPngquant({
+				quality: [0.6, 0.8],
+			}),
+		],
+	});
+
+	console.log(files);
+}
+
 module.exports = uploadRouter;
